a792eb1cc889e69f763a11c29422e3f1bad0b3bf
Kevin Fallas <kev.fallas@gmail.com>
2023-06-12 14:17:34 -0600

    Add env file

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3030",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-type": "application/json"
   }
