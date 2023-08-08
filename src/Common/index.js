c696865c73b76af0f2f3bef02cc30bb8125c8c09
Kevin Fallas <kev.fallas@gmail.com>
2023-08-08 11:43:46 -0600

    Move nav bar to layout folder

--- src/Common/index.js
+++ src/Common/index.js
@@ -3,7 +3,6 @@ export { default as DeleteAlertModal } from './DeleteAlertModal/DeleteAlertModal
 export { default as ErrorPage } from './ErrorPage/ErrorPage.component';
 export { default as SnackBar } from './SnackBar/SnackBar.component';
 export { default as Spinner } from './Spinner/Spinner.component';
-export { default as NavBar } from './NavBar/NavBar.container';
 export { default as MoreButton } from './MoreButton/MoreButton.component';
 export { default as Tooltip } from './Tooltip/Tooltip.component';
 export { default as InfoTooltip } from './InfoTooltip/InfoTooltip.component';
