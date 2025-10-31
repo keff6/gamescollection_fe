7ae8347b44dd46d66776618770c09d0bc9e79068
Kevin Fallas <kev.fallas@gmail.com>
2025-10-31 12:16:25 -0600

    Fix on error messages

--- src/Layout/Footer/Footer.component.jsx
+++ src/Layout/Footer/Footer.component.jsx
@@ -5,7 +5,7 @@ import classes from './Footer.module.css'
 const Footer = () => (
   <div className={classes.footerContainer}>
     <div className={classes.footerContent}>
-      <span className={classes.nameLabel}>2025 Kevin Fallas</span>
+      <span className={classes.nameLabel}>© 2025 Kevin Fallas</span>
       <div className={classes.divider}></div>
       <div className={classes.iconsContainer}>
         <Nav.Link href="https://www.linkedin.com/in/kevin-fallas/" target="_blank"><Linkedin /></Nav.Link>
--- src/hooks/useApiErrorHandler.js
+++ src/hooks/useApiErrorHandler.js
@@ -9,7 +9,8 @@ const useApiErrorHandler = (error) => {
   useEffect(() => {
     if (error) {
       const errorCode = error?.response?.data || "";
-      let message = errorCode === ERROR_CODES.IS_REFERENCED ? "Cannot delete! It has dependencies" : error.message;
+      let message = errorCode === ERROR_CODES.IS_REFERENCED ? "Cannot delete! It has dependencies" :
+      (errorCode !== "") ? errorCode : error.message;
       
       openSnackbar({message: message || "Something went wrong!", type: OPERATION_OUTCOME.FAILED});
     }
