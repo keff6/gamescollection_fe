e266ed778c7ec16e74e48629975a870d22848148
Kevin Fallas <kev.fallas@gmail.com>
2025-10-30 18:54:53 -0600

    Fix toggle menu expanded issue

--- src/Layout/NavBar/NavBar.component.jsx
+++ src/Layout/NavBar/NavBar.component.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Joystick } from "react-bootstrap-icons";
@@ -6,8 +7,20 @@ import { useAppState } from '../../hooks';
 import classes from './NavBar.module.css';
 
 const NavBar = ({ logOut }) =>{
+  const [isExpanded, setIsExpanded] = useState(false);
+  const menuRef = useRef();
   const { user } = useAppState();
   const currentUser = user || null;
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsExpanded(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [menuRef]);
 
   const getInitialsCircle = () => (
     <div className={classes.circle}>
@@ -19,24 +32,33 @@ const NavBar = ({ logOut }) =>{
   )
 
   return (
-    <Navbar collapseOnSelect expand="lg"  variant="dark" className={`fixed-top ${classes.bgDark}`}>
+    <Navbar
+      ref={menuRef}
+      collapseOnSelect
+      expand="lg"
+      variant="dark"
+      className={`fixed-top ${classes.bgDark}`}
+      expanded={isExpanded}
+    >
       <Container>
         <Navbar.Brand as={Link} to="/">
-          
           <div className={classes.logoContainer}>
           <Joystick /><span>Games Collection</span>
           </div>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => setIsExpanded(isExpanded ? false : true)}
+        />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/brands">Brands</Nav.Link>
-            <Nav.Link as={Link} to="/export">Export</Nav.Link>
+            <Nav.Link as={Link} to="/" onClick={() => setIsExpanded(false)}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/brands" onClick={() => setIsExpanded(false)}>Brands</Nav.Link>
+            <Nav.Link as={Link} to="/export" onClick={() => setIsExpanded(false)}>Export</Nav.Link>
             {currentUser && <NavDropdown title="Admin" id="collasible-nav-dropdown">
-              <NavDropdown.Item as={Link} to="/brands">Brands</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/genres">Genres</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to="/export">Export</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/brands" onClick={() => setIsExpanded(false)}>Brands</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/genres" onClick={() => setIsExpanded(false)}>Genres</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/export" onClick={() => setIsExpanded(false)}>Export</NavDropdown.Item>
             </NavDropdown>}
           </Nav>
           <div className={`${classes.divider} d-block d-md-none`} />
@@ -58,7 +80,7 @@ const NavBar = ({ logOut }) =>{
                   <NavDropdown.Item onClick={logOut}>Log out</NavDropdown.Item>
                 </NavDropdown>
               :
-              <Nav.Link as={Link} to="/login">Log In</Nav.Link>
+              <Nav.Link as={Link} to="/login" onClick={() => setIsExpanded(false)}>Log In</Nav.Link>
             }
           </Nav>
         </Navbar.Collapse>
